c9b8d306dcd9ba89e5e02ba9b19fc20429a5c2d3
TheCreeper669 <thecreeper669@gmail.com>
2020-03-16 11:22:55 +0100

    re init

--- __main__.py
+++
@@ -1,8 +0,0 @@
-from game import *
-
-pg.init()
-game = Game()
-game.start()
-pg.quit()
-
-
--- pygamepp/__init__.py
+++
@@ -1,8 +0,0 @@
-import pygame
-pg = pygame
-
-import pygamepp.time as time
-import pygamepp.key as key
-import pygamepp.json as json
-
-
